2184d94dc8ab6537432c3c76eef19408a986e2fa
LK <yulei198903@foxmail.com>
2017-01-07 21:25:27 +0800

    添加top导航栏

---
+++ js/common/top.js
@@ -0,0 +1,12 @@
+
+window.onload = function(){
+	
+	var logo = document.getElementsByClassName("logo")[0];
+	
+	logo.onmouseover = function(){
+		this.style.animation = "logoBigAnimation 0.2s linear 0 1 forwards";		
+	}
+	logo.onmouseout = function(){
+		this.style.animation = "logoSmallAnimation 0.2s linear 0 1 forwards";
+	}
+}

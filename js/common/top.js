92d69164fa8b348470682ae92c6f9f43a681f067
LK <yulei198903@foxmail.com>
2017-01-08 00:40:11 +0800

    添加搞笑小人

--- js/common/top.js
+++ js/common/top.js
@@ -2,11 +2,23 @@
 window.onload = function(){
 	
 	var logo = document.getElementsByClassName("logo")[0];
-	
 	logo.onmouseover = function(){
 		this.style.animation = "logoBigAnimation 0.2s linear 0 1 forwards";		
 	}
 	logo.onmouseout = function(){
 		this.style.animation = "logoSmallAnimation 0.2s linear 0 1 forwards";
 	}
+	var bq = document.getElementsByClassName("bq")[0];
+	bq.onmouseover = function(){
+		this.setAttribute('src','img/bq01.png');
+	}
+	bq.onmouseout = function(){
+		if(Math.random()*10 > 8){
+			this.setAttribute('src','img/bq02.png');	
+		}else{
+			this.setAttribute('src','img/bq03.png');
+		}
+		
+	}
+	
 }

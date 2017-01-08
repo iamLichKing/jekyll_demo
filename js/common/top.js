118045e2dc56cc1802bd559f7e4df6d9af2e8f8a
LK <yulei198903@foxmail.com>
2017-01-08 19:11:13 +0800

    添加文字颜色和封装动画

--- js/common/top.js
+++ js/common/top.js
@@ -3,11 +3,13 @@ window.onload = function(){
 	
 	var logo = document.getElementsByClassName("logo")[0];
 	logo.onmouseover = function(){
+		console.log("1");
 		this.style.animation = "logoBigAnimation 0.2s linear 0 1 forwards";		
 	}
 	logo.onmouseout = function(){
 		this.style.animation = "logoSmallAnimation 0.2s linear 0 1 forwards";
 	}
+	
 	var bq = document.getElementsByClassName("bq")[0];
 	bq.onmouseover = function(){
 		this.setAttribute('src','img/bq01.png');
@@ -17,8 +19,17 @@ window.onload = function(){
 			this.setAttribute('src','img/bq02.png');	
 		}else{
 			this.setAttribute('src','img/bq03.png');
+		}	
+	}
+	
+	var nav_texts = document.getElementsByClassName("nav-text");
+	for(let i = 0; i < nav_texts.length; i++){
+		nav_texts[i].onmouseover = function(){
+			nav_texts[i].style.color = "red";
 		}
-		
+		nav_texts[i].onmouseout = function(){
+			nav_texts[i].style.color = "#000000";
+		}
 	}
 	
 }

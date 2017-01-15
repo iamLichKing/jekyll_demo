73cf068ddbb29fd8d02318a1ac2be9dc51ef2869
LK <yulei198903@foxmail.com>
2017-01-15 19:00:51 +0800

    重新设计首页

--- js/common/top.js
+++ js/common/top.js
@@ -1,33 +1,8 @@
 
-window.onload = function(){
-	
-	var logo = document.getElementsByClassName("logo")[0];
-	logo.onmouseover = function(){
-		this.style.animation = "logoBigAnimation 0.2s linear 0 1 forwards";		
-	}
-	logo.onmouseout = function(){
-		this.style.animation = "logoSmallAnimation 0.2s linear 0 1 forwards";
-	}
-	
-	var bq = document.getElementsByClassName("bq")[0];
-	bq.onmouseover = function(){
-		this.setAttribute('src','img/bq01.png');
-	}
-	bq.onmouseout = function(){
-		if(Math.random()*10 > 8){
-			this.setAttribute('src','img/bq02.png');	
-		}else{
-			this.setAttribute('src','img/bq03.png');
-		}
-	}
-	
-	var nav_texts = document.getElementsByClassName("nav-text");
-	for(let i = 0; i < nav_texts.length; i++){
-		nav_texts[i].onmouseover = function(){
-			nav_texts[i].style.color = "green";
-		}
-		nav_texts[i].onmouseout = function(){
-			nav_texts[i].style.color = "#000000";
-		}
-	}	
+var logo = document.getElementsByClassName("logo")[0];
+logo.onmouseover = function(){
+	this.style.animation = "logoBigAnimation 0.2s linear 0 1 forwards";		
+}
+logo.onmouseout = function(){
+	this.style.animation = "logoSmallAnimation 0.2s linear 0 1 forwards";
 }
--- js/home/home.js
+++ js/home/home.js
@@ -1,11 +1,7 @@
 
-window.onload = function(){
-	var vido = document.getElementById('vido');	
-	var audo = document.getElementById('audo');
-	var banner = document.getElementsByClassName('banner')[0];
-	vido.onended = function(){
-		this.style.display = "none";
-		banner.style.display = "block";
-		window.audo.play();
-	}
-}
+
+var audo  = document.getElementById('audo');
+audo.loop = 'true';
+audo.play();
+
+
